6308a226f094353d7c652dbed5e1bd0b1a1e0e7c
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-07-24 17:53:46 +0900

    [feat/#6] zip 페이지 오류 해결

--- app/src/main/java/umc/link/zip/Zip_Item_Fragment.kt
+++ app/src/main/java/umc/link/zip/Zip_Item_Fragment.kt
@@ -34,7 +34,7 @@ class Zip_Item_Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_zip__item_, container, false)
+        return inflater.inflate(R.layout.fragment_zip, container, false)
     }
 
     companion object {
