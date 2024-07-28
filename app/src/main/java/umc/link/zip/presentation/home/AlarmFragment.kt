c62311a0cf86533f7a62b0447a0c2df81953d6c9
다희 <163154446+ekgml001@users.noreply.github.com>
2024-07-28 12:31:32 +0900

    [Feat/#14] Alarm 페이지 구현
    * [feat/#14] 알림 페이지 틀 구현
    
    1. item_alarm 기본 틀 구현
    2. fragment_alarm (알림 O) 구현
    3. fragment_alarm_none (알림 X) 구현
    4. AlarmFragment에 아이템 사이 구분선 넣는 코드 작성
    
    * 알림 페이지 xml 구현 완료
    
    * [Feat/#14] 알림페이지 구현
    
    * [FIX/#14] fragment 구조 수정
    
    * [FIX/#14] 툴바, 알림 유무별 visibility 수정
    
    * [FIX/#14] text size 수정
    
    * [FIX/#14] item icon size 수정
    
    * [FIX/#14] clip 아이콘 위치 수정
    
    * [FIX/#14] recycler view 바텀 잡음

---
+++ app/src/main/java/umc/link/zip/presentation/home/AlarmFragment.kt
@@ -0,0 +1,17 @@
+package umc.link.zip.presentation.create
+
+import dagger.hilt.android.AndroidEntryPoint
+import umc.link.zip.R
+import umc.link.zip.databinding.FragmentAlarmBinding
+import umc.link.zip.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class AlarmFragment : BaseFragment<FragmentAlarmBinding>(R.layout.fragment_alarm){
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
