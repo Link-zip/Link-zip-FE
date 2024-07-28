9770bb205c16e27b26f5602168f5dc372d648f68
다희 <163154446+ekgml001@users.noreply.github.com>
2024-07-28 12:52:10 +0900

    [Feat/#8] Text & Link 저장 페이지 구현
    * [Feat/#8] 텍스트요약/링크저장 페이지 구현
    
    * [MOD/#8] 전체적인 틀 잡기 완료
    
    * [MOD/#8] 비율코딩 & 툴바/버튼 수정
    
    * [FIX/#8] fragment 오류 수정
    
    * [FIX/#8] text size, 버튼 그림자 수정
    
    * [FIX/#8] fragment_savelink_alarm 버튼 수정

---
+++ app/src/main/java/umc/link/zip/presentation/create/CustomtextMemoFragment.kt
@@ -0,0 +1,17 @@
+package umc.link.zip.presentation.create
+
+import dagger.hilt.android.AndroidEntryPoint
+import umc.link.zip.R
+import umc.link.zip.databinding.FragmentCustomtextMemoBinding
+import umc.link.zip.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class CustomtextMemoFragment : BaseFragment<FragmentCustomtextMemoBinding>(R.layout.fragment_customtext_memo){
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
---
+++ app/src/main/java/umc/link/zip/presentation/create/CustomtextZipFragment.kt
@@ -0,0 +1,17 @@
+package umc.link.zip.presentation.create
+
+import dagger.hilt.android.AndroidEntryPoint
+import umc.link.zip.R
+import umc.link.zip.databinding.FragmentCustomtextZipBinding
+import umc.link.zip.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class CustomtextZipFragment : BaseFragment<FragmentCustomtextZipBinding>(R.layout.fragment_customtext_zip){
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
