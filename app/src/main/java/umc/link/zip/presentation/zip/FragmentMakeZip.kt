3452c6626d6b541accd7a6f99491cbd7f32da685
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-08-01 02:13:11 +0900

    [feat/#26] fragment_makezip.xml의 zip 색상 변경 구현

--- app/src/main/java/umc/link/zip/presentation/zip/FragmentMakeZip.kt
+++ app/src/main/java/umc/link/zip/presentation/zip/FragmentMakeZip.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import umc.link.zip.R
@@ -27,6 +28,37 @@ class FragmentMakeZip : Fragment() {
             // FragmentZip으로 돌아감
             findNavController().navigateUp()
         }
+
+        // Get the ImageView that will change drawable
+        val fragmentMakezipExzipIc: ImageView = view.findViewById(R.id.fragment_makezip_exzip_ic)
+
+        // Set click listeners for each rectangle
+        view.findViewById<View>(R.id.rectangle_1).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_1)
+        }
+
+        view.findViewById<View>(R.id.rectangle_2).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_2)
+        }
+
+        view.findViewById<View>(R.id.rectangle_3).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_3)
+        }
+
+        view.findViewById<View>(R.id.rectangle_4).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_4)
+        }
+
+        view.findViewById<View>(R.id.rectangle_5).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_5)
+        }
+
+        view.findViewById<View>(R.id.rectangle_6).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_6)
+        }
+
+        view.findViewById<View>(R.id.rectangle_7).setOnClickListener {
+            fragmentMakezipExzipIc.setImageResource(R.drawable.ic_zip_shadow_7)
+        }
     }
 }
-
