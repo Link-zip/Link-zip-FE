040581011c48ff25bd8e7bef1009b6c43e0dea38
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-08-04 12:54:07 +0900

    [ADD/#32] 링크 수정 kotlin 파일 추가

---
+++ app/src/main/java/umc/link/zip/presentation/create/LinkEditFragment.kt
@@ -0,0 +1,18 @@
+package umc.link.zip.presentation.create
+
+import dagger.hilt.android.AndroidEntryPoint
+import umc.link.zip.R
+import umc.link.zip.databinding.FragmentLinkEditBinding
+import umc.link.zip.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class LinkEditFragment : BaseFragment<FragmentLinkEditBinding>(R.layout.fragment_link_edit){
+
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
