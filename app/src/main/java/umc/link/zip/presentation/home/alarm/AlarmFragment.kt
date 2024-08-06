a65f2927cc7d2880ef0f934019f0281d939b36c1
ekgml001 <dahea5069@gmail.com>
2024-08-07 07:12:13 +0900

    [MOD/#27] 더미데이터 수정

--- app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmFragment.kt
+++ app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmFragment.kt
@@ -2,6 +2,7 @@ package umc.link.zip.presentation.home.alarm
 
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import umc.link.zip.R
 import umc.link.zip.databinding.FragmentAlarmBinding
@@ -27,7 +28,17 @@ class AlarmFragment : BaseFragment<FragmentAlarmBinding>(R.layout.fragment_alarm
     }
 
     override fun initView() {
+        // 업버튼
+        binding.ivAlarmToolbarBack.setOnClickListener{
+            navigateToHome()
+        }
+
+        // 어댑터
         adapter = AlarmAdapter()
         binding.profilePostRv.adapter = adapter
+    }
+
+    private fun navigateToHome() {
+        findNavController().navigate(R.id.action_alarmFragment_to_homeFragment)
     }
 }
--- app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmRepository.kt
+++ app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmRepository.kt
@@ -7,12 +7,31 @@ import javax.inject.Inject
 class AlarmRepository @Inject constructor(){
     fun getAlarms(): LiveData<List<Alarm>> {
         val alarms = MutableLiveData<List<Alarm>>()
-        // 더미 데이터 추가
+
+        // 더미 데이터
         alarms.value = listOf(
-                Alarm(1, 0, "original", "2024-03-20T08:00:00Z", "아침 알람", "아침 알람 내용입니다."),
-                Alarm(2, 1, "reminder", "2024-03-20T12:00:00Z", "점심 알람", "점심 알람 내용입니다."),
-                Alarm(3, 0, "original", "2024-03-20T18:00:00Z", "저녁 알람", "저녁 알람 내용입니다.")
+                Alarm(
+                        1,
+                        0,
+                        "original",
+                        "2024-03-20T08:00:00Z",
+                        "\"마이크로/나노 인플루언서 마케팅 전략\" 리마인드 알림이 도착했어요!",
+                        "트랜드 파악!"),
+                Alarm(
+                        2,
+                        1,
+                        "reminder",
+                        "2024-03-20T12:00:00Z",
+                        "어쩌고 저쩌고",
+                        "어쩌고 메모"),
+                Alarm(3,
+                        0,
+                        "original",
+                        "2024-03-20T18:00:00Z",
+                        "제목제목",
+                        "메모메모")
         )
+
         return alarms
     }
 }
