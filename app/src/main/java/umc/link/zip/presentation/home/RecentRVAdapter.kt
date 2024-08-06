9840715d1c506d173b5c198bae9ff7109d719d9a
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2024-08-06 23:56:12 +0900

    [Feat/#31] home recyclerview (#36)
    * [FEAT/#31] Recycler View 구현(데이터는 하드코딩)
    
    * [MOD/#31] 추천 사이트 RecyclerView를 ScrollView로 변경
    
    * [DEL/#31] RecyclerView를 ScrollView로 변경하면서 필요없어진 파일 삭제
    
    * [MOD/#31] iv 이미지로딩 glide로 변경 & Zip data class 추가
    
    * [ADD/#31] Home 상태창 투명 & 블러 추가
    
    * [ADD/#31] toolbar 배경색 추가 & navigation 설정
    
    * [MOD/#31] home blur 삭제 및 카드이미지 여백 수정
    
    * [MOD/#31] ListFragment로 이동시 데이터 넘기는 방식 변경
    
    * [MOD/#31] 카드 이미지 margin 수정
    
    * [MOD/#33] 최근 저장한 링크, 추천 사이트 margin 조정
    
    ---------
    
    Co-authored-by: Timeisfast <seongyungim99@gamil.com>

---
+++ app/src/main/java/umc/link/zip/presentation/home/RecentRVAdapter.kt
@@ -0,0 +1,33 @@
+package umc.link.zip.presentation.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import umc.link.zip.databinding.ItemHomeRecentBinding
+import umc.link.zip.domain.model.Link
+
+class RecentRVAdapter(private val recentList: ArrayList<Link>, private val context: Context)
+    : RecyclerView.Adapter<RecentRVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentRVAdapter.ViewHolder {
+        val binding: ItemHomeRecentBinding = ItemHomeRecentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecentRVAdapter.ViewHolder, position: Int) {
+        holder.bind(recentList[position])
+    }
+
+    override fun getItemCount(): Int = recentList.size
+
+    inner class ViewHolder(val binding: ItemHomeRecentBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(link: Link) {
+            Glide.with(context)
+                .load(link.thumbnail)
+                .into(binding.ivItemHomeLinkImg)
+            binding.tvItemHomeLinkTitle.text = link.title
+        }
+    }
+}
