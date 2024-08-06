82550bb826183e50f330dfdeb3611cce319d08a4
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2024-08-06 23:56:12 +0900

    [Feat/#31] home recyclerview (#36)
    * [FEAT/#31] Recycler View 구현(데이터는 하드코딩)
    
    * [MOD/#31] 추천 사이트 RecyclerView를 ScrollView로 변경
    
    * [DEL/#31] RecyclerView를 ScrollView로 변경하면서 필요없어진 파일 삭제
    
    * [MOD/#31] iv 이미지로딩 glide로 변경 & Zip data class 추가
    
    * [ADD/#31] Home 상태창 투명 & 블러 추가
    
    * [ADD/#31] toolbar 배경색 추가 & navigation 설정
    
    * [MOD/#31] home blur 삭제 및 카드이미지 여백 수정
    
    * [MOD/#31] ListFragment로 이동시 데이터 넘기는 방식 변경
    
    * [MOD/#31] 카드 이미지 margin 수정
    
    * [MOD/#33] 최근 저장한 링크, 추천 사이트 margin 조정
    
    ---------
    
    Co-authored-by: Timeisfast <seongyungim99@gamil.com>

--- app/src/main/java/umc/link/zip/presentation/base/BaseActivity.kt
+++ app/src/main/java/umc/link/zip/presentation/base/BaseActivity.kt
@@ -1,8 +1,12 @@
 package umc.link.zip.presentation.base
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -18,7 +22,19 @@ abstract class BaseActivity<V : ViewDataBinding>(@LayoutRes val layoutResource: 
         _binding = DataBindingUtil.setContentView(this, layoutResource)
         binding.lifecycleOwner = this
         setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+        window.statusBarColor = Color.TRANSPARENT
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+
         initObserver()
         initView()
+    }
+
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val winAttr = window.attributes
+        winAttr.flags = if(on) winAttr.flags or bits else winAttr.flags and bits.inv()
+        window.attributes = winAttr
     }
 }
