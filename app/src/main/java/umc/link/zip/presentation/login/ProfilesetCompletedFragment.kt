00f450d7d2f956879eac6768066945d7bb242bc9
Timeisfast <seongyungim99@gamil.com>
2024-08-07 23:22:04 +0900

    [MOD/#33] 프로필 설정 완료 페이지 닉네임 폰트 변경

--- app/src/main/java/umc/link/zip/presentation/login/ProfilesetCompletedFragment.kt
+++ app/src/main/java/umc/link/zip/presentation/login/ProfilesetCompletedFragment.kt
@@ -1,21 +1,27 @@
 package umc.link.zip.presentation.login
 
 import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.TypefaceSpan
 import androidx.activity.OnBackPressedCallback
+import androidx.annotation.RequiresApi
+import androidx.core.content.res.ResourcesCompat
 import dagger.hilt.android.AndroidEntryPoint
 import umc.link.zip.R
 import umc.link.zip.databinding.FragmentProfilesetcompletedBinding
 import umc.link.zip.presentation.MainActivity
 import umc.link.zip.presentation.base.BaseFragment
 
+@RequiresApi(Build.VERSION_CODES.P)
 @AndroidEntryPoint
 class ProfilesetCompletedFragment : BaseFragment<FragmentProfilesetcompletedBinding>(R.layout.fragment_profilesetcompleted){
     override fun initObserver() {
 
     }
+
 
     override fun initView() {
         setClickListener()
@@ -36,8 +42,12 @@ class ProfilesetCompletedFragment : BaseFragment<FragmentProfilesetcompletedBind
         val nickname = arguments?.getString("nickname") ?: "익명"
         val spannableString = SpannableString("$nickname 님, Link.zip에\n오신 걸 환영해요!")
 
+        val myTypeface = Typeface.create(
+            ResourcesCompat.getFont(requireContext(), R.font.pretendard_semibold),
+            Typeface.BOLD
+        )
         spannableString.setSpan(
-            TypefaceSpan("pretendard_semibold"),
+            TypefaceSpan(myTypeface),
             0,
             nickname.length,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
