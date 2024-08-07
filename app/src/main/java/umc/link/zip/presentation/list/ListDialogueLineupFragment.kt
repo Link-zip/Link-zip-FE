03cf3eefd0db42ff3ad92bb64ae6ebd306026117
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-08-08 06:07:30 +0900

    [Feat/#29] lineup dialog 기능 구현
    - 프래그먼트 간 데이터 주고받기 구현 (Viewmodel 사용)
    - 초기 버튼 세팅을 위해 변수 선언 및 버튼 클릭시 뷰모델 세팅
    - collectLatest로 데이터 가져오기 / emit으로 저장(?)

--- app/src/main/java/umc/link/zip/presentation/list/ListDialogueLineupFragment.kt
+++ app/src/main/java/umc/link/zip/presentation/list/ListDialogueLineupFragment.kt
@@ -1,31 +1,122 @@
 package umc.link.zip.presentation.list
 
 import android.app.Dialog
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import umc.link.zip.R
 import umc.link.zip.databinding.FragmentDialogueLineupBinding
 import umc.link.zip.presentation.base.BaseBottomSheetDialogFragment
 
-class ListDialogueLineupFragment : BaseBottomSheetDialogFragment<FragmentDialogueLineupBinding>(R.layout.fragment_dialogue_lineup){
+@AndroidEntryPoint
+class ListDialogueLineupFragment : BaseBottomSheetDialogFragment<FragmentDialogueLineupBinding>(R.layout.fragment_dialogue_lineup) {
+
+    private val listUnreadDialogSharedViewModel: ListUnreadDialogSharedViewModel by activityViewModels()
 
     override fun getTheme(): Int {
-        return R.style.BottomSheetDialogTheme // 정의한 스타일 사용
+        return R.style.BottomSheetDialogTheme
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState)
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         return dialog
     }
 
-    override fun initObserver() {
-
-    }
+    override fun initObserver() {}
 
     override fun initView() {
-
+        setStart()
+        setOnClick()
     }
 
+    private fun setStart() {
+        // 기존 데이터 가져오기 위함
+        reset()
+        lifecycleScope.launch {
+            listUnreadDialogSharedViewModel.selectedData.collectLatest {
+                data ->
+                when (data) {
+                    "latest" -> {
+                        selected(binding.tvViewDialogueLineupItem1, binding.viewDialogueLineupChkLatest)
+                    }
+                    "oldest" -> {
+                        selected(binding.tvViewDialogueLineupItem2, binding.viewDialogueLineupChkOldest)
+                    }
+                    "ganada" -> {
+                        selected(binding.tvViewDialogueLineupItem3, binding.viewDialogueLineupChkGanada)
+                    }
+                    "visit" -> {
+                        selected(binding.tvViewDialogueLineupItem4, binding.viewDialogueLineupChkVisit)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setOnClick() {
+        binding.clDialogueLineupItem1.setOnClickListener {
+            lifecycleScope.launch {
+                reset()
+                selected(binding.tvViewDialogueLineupItem1, binding.viewDialogueLineupChkLatest)
+                listUnreadDialogSharedViewModel.setSelectedData("latest")
+            }
+        }
+        binding.clDialogueLineupItem2.setOnClickListener {
+            lifecycleScope.launch {
+                reset()
+                selected(binding.tvViewDialogueLineupItem2, binding.viewDialogueLineupChkOldest)
+                listUnreadDialogSharedViewModel.setSelectedData("oldest")
+            }
+        }
+        binding.clDialogueLineupItem3.setOnClickListener {
+            lifecycleScope.launch {
+                reset()
+                selected(binding.tvViewDialogueLineupItem3, binding.viewDialogueLineupChkGanada)
+                listUnreadDialogSharedViewModel.setSelectedData("ganada")
+            }
+        }
+        binding.clDialogueLineupItem4.setOnClickListener {
+            lifecycleScope.launch {
+                reset()
+                selected(binding.tvViewDialogueLineupItem4, binding.viewDialogueLineupChkVisit)
+                listUnreadDialogSharedViewModel.setSelectedData("visit")
+            }
+        }
+    }
+
+    private fun selected(tv: TextView, view: View) {
+        tv.setTextColor(ContextCompat.getColor(binding.root.context, R.color.black))
+        view.visibility = View.VISIBLE
+    }
+
+    private fun reset() {
+        binding.tvViewDialogueLineupItem1.setTextColor(ContextCompat.getColor(binding.root.context, R.color.nav_selected))
+        binding.tvViewDialogueLineupItem2.setTextColor(ContextCompat.getColor(binding.root.context, R.color.nav_selected))
+        binding.tvViewDialogueLineupItem3.setTextColor(ContextCompat.getColor(binding.root.context, R.color.nav_selected))
+        binding.tvViewDialogueLineupItem4.setTextColor(ContextCompat.getColor(binding.root.context, R.color.nav_selected))
+        binding.viewDialogueLineupChkLatest.visibility = View.INVISIBLE
+        binding.viewDialogueLineupChkOldest.visibility = View.INVISIBLE
+        binding.viewDialogueLineupChkGanada.visibility = View.INVISIBLE
+        binding.viewDialogueLineupChkVisit.visibility = View.INVISIBLE
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        lifecycleScope.launch {
+            listUnreadDialogSharedViewModel.dismissDialog()
+        }
+    }
 }
--- app/src/main/java/umc/link/zip/presentation/list/ListUnreadFragment.kt
+++ app/src/main/java/umc/link/zip/presentation/list/ListUnreadFragment.kt
@@ -2,8 +2,15 @@ package umc.link.zip.presentation.list
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import umc.link.zip.presentation.base.BaseFragment
 
 import umc.link.zip.R
@@ -17,6 +24,10 @@ class ListUnreadFragment : BaseFragment<FragmentListRvBinding>(R.layout.fragment
         findNavController()
     }
     private val viewModel: ListUnreadViewModel by viewModels({requireParentFragment()})
+
+    private val listUnreadDialogSharedViewModel: ListUnreadDialogSharedViewModel by activityViewModels()
+
+    private var userSelectedLineup = "latest"
     private val listUnreadRVA by lazy {
         ListUnreadRVA{
             /* 링크 페이지 연결
@@ -28,40 +39,90 @@ class ListUnreadFragment : BaseFragment<FragmentListRvBinding>(R.layout.fragment
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initView()
-        initObserver()
+    override fun initObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            listUnreadDialogSharedViewModel.selectedData.collectLatest { data ->
+                userSelectedLineup = data
+                setLineupOnDialog(userSelectedLineup)
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            listUnreadDialogSharedViewModel.dialogDismissed.collectLatest { dismissed ->
+                if (dismissed) {
+                    setLineupDismissDialog(userSelectedLineup)
+                    listUnreadDialogSharedViewModel.resetDialogDismissed()
+                }
+            }
+        }
+    }
+
+    private fun setLineupOnDialog(selected: String) {
+        when (selected) {
+            "latest" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_early_selected))
+            }
+            "oldest" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_old_selected))
+            }
+            "ganada" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_ganada_selected))
+            }
+            "visit" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_visit_selected))
+            }
+        }
+    }
+
+    private fun setLineupDismissDialog(selected: String) {
+        when (selected) {
+            "latest" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_early_unselected))
+            }
+            "oldest" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_old_unselected))
+            }
+            "ganada" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_ganada_unselected))
+            }
+            "visit" -> {
+                binding.ivListRvDrawerbtnLineup.setImageDrawable(ContextCompat.getDrawable(binding.ivListRvDrawerbtnLineup.context, R.drawable.drawerbtn_lineup_visit_unselected))
+            }
+        }
+    }
+
+    override fun initView() {
+        initPostListRVAdapter()
         setupClickListener()
     }
 
-    override fun initObserver() {
-    }
-    override fun initView() {
-        initPostListRVAdapter()
-    }
-    private fun initPostListRVAdapter(){
+    private fun initPostListRVAdapter() {
         binding.rvList.adapter = listUnreadRVA
         val zip = Zip("1", "Zip Title", "blue")
         val zip2 = Zip("1", "인사이트", "yellow")
         val list = listOf(
-            Link("1", "테스트입니다1", "url", "텍스트", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff",1, "2024.7.28", zip),
-            Link("2", "마이크로/나노 인플루언서 마케팅 전략 ## 최대 2줄", "url", "", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff",0, "2024.7.29", zip2),
-            Link("3", "테스트입니다3", "url", "텍스트", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff",1, "2024.7.30", zip),
-            Link("4", "테스트입니다4", "url", "", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff",0, "2024.7.31", zip),
-            Link("5", "테스트입니다5", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(),1, "2024.7.28", zip),
-            Link("6", "테스트입니다6", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(),1, "2024.7.29", zip),
-            Link("7", "테스트입니다7", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(),1, "2024.7.30", zip),
-            Link("8", "테스트입니다8", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(),1, "2024.7.31", zip)
+            Link("1", "테스트입니다1", "url", "텍스트", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff", 1, "2024.7.28", zip),
+            Link("2", "마이크로/나노 인플루언서 마케팅 전략 ## 최대 2줄", "url", "", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff", 0, "2024.7.29", zip2),
+            Link("3", "테스트입니다3", "url", "텍스트", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff", 1, "2024.7.30", zip),
+            Link("4", "테스트입니다4", "url", "", "https://i.scdn.co/image/ab67616d0000b2734ed058b71650a6ca2c04adff", 0, "2024.7.31", zip),
+            Link("5", "테스트입니다5", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(), 1, "2024.7.28", zip),
+            Link("6", "테스트입니다6", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(), 1, "2024.7.29", zip),
+            Link("7", "테스트입니다7", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(), 1, "2024.7.30", zip),
+            Link("8", "테스트입니다8", "url", "텍스트", R.drawable.btn_bottomnav_create.toString(), 1, "2024.7.31", zip)
         )
         listUnreadRVA.submitList(list)
     }
-    private fun setupClickListener() { // 전체
+
+    private fun setupClickListener() {
         binding.ivListRvDrawerbtnLineup.setOnClickListener {
+            setLineupOnDialog(userSelectedLineup)
+            viewLifecycleOwner.lifecycleScope.launch {
+                listUnreadDialogSharedViewModel.setSelectedData(userSelectedLineup)
+            }
             val dialogFragment = ListDialogueLineupFragment()
             dialogFragment.show(parentFragmentManager, "ListDialogueLineupFragment")
         }
-        binding.ivListRvDrawerbtnAll.setOnClickListener { //최신순
+        binding.ivListRvDrawerbtnAll.setOnClickListener {
             val dialogFragment = ListDialogueListselectFragment()
             dialogFragment.show(parentFragmentManager, "ListDialogueListselectFragment")
         }
