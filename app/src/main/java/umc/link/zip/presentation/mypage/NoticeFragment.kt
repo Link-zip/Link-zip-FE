bf2574a1854c05bc32c6672745656c721408e56d
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-08-08 04:27:57 +0900

    [Feat/#34] 공지사항 리사이클러뷰 구현

--- app/src/main/java/umc/link/zip/presentation/mypage/NoticeFragment.kt
+++ app/src/main/java/umc/link/zip/presentation/mypage/NoticeFragment.kt
@@ -1,10 +1,14 @@
 package umc.link.zip.presentation.mypage
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import umc.link.zip.R
 import umc.link.zip.databinding.FragmentNoticeBinding
+import umc.link.zip.domain.model.notice.Notice
 import umc.link.zip.presentation.base.BaseFragment
+import umc.link.zip.presentation.mypage.adapter.NoticeRVA
 
 class NoticeFragment :BaseFragment<FragmentNoticeBinding> (R.layout.fragment_notice){
     private val navigator by lazy { findNavController() }
@@ -16,6 +20,33 @@ class NoticeFragment :BaseFragment<FragmentNoticeBinding> (R.layout.fragment_not
         binding.ivNoticeToolbarBack.setOnClickListener{
             navigator.navigateUp()
         }
+        initNoticeRVAdapter()
+    }
+
+    private val noticeRVA by lazy {
+        NoticeRVA{
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initObserver()
+        setupClickListener()
+    }
+
+    private fun initNoticeRVAdapter(){
+        binding.rvNotice.adapter = noticeRVA
+        val list = listOf(
+            Notice("1", "공지사항 제목", "2024.06.29"),
+            Notice("2", "공지사항 제목2", "2024.06.29"),
+            Notice("3", "공지사항 제목3", "2024.06.29"),
+            Notice("4", "공지사항 제목4", "2024.06.29"),
+        )
+        noticeRVA.submitList(list)
+    }
+    private fun setupClickListener() {
+
     }
 
 }
