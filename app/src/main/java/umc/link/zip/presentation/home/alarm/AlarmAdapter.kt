606f21d5de877e074e9aa00000258f78e47e51db
ekgml001 <dahea5069@gmail.com>
2024-08-07 22:23:06 +0900

    [ADD/#27] 알림 별 조건문 추가

--- app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmAdapter.kt
+++ app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmAdapter.kt
@@ -1,6 +1,7 @@
 package umc.link.zip.presentation.home.alarm
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -22,6 +23,42 @@ class AlarmAdapter : ListAdapter<Alarm, AlarmAdapter.AlarmViewHolder>(AlarmDiffC
         fun bind(alarm: Alarm) {
             binding.alarm = alarm
             binding.executePendingBindings()
+
+            // 아이콘 visibility 설정
+            setIconVisibility(alarm)
+        }
+
+        private fun setIconVisibility(alarm: Alarm) {
+            when (alarm.alarm_type) {
+                "original" -> {
+                    if (alarm.alarm_status == 0) {
+                        binding.ivItemAlarmOriginal0Icon.visibility = View.VISIBLE
+                        binding.ivItemAlarmOriginal1Icon.visibility = View.INVISIBLE
+                    } else {
+                        binding.ivItemAlarmOriginal0Icon.visibility = View.INVISIBLE
+                        binding.ivItemAlarmOriginal1Icon.visibility = View.VISIBLE
+                    }
+                    binding.ivItemAlarmRemind0Icon.visibility = View.INVISIBLE
+                    binding.ivItemAlarmRemind1Icon.visibility = View.INVISIBLE
+
+                    binding.tvItemAlarmTypeOriginal.visibility = View.VISIBLE
+                    binding.tvItemAlarmTypeRemind.visibility = View.INVISIBLE
+                }
+                "reminder" -> {
+                    if (alarm.alarm_status == 0) {
+                        binding.ivItemAlarmRemind0Icon.visibility = View.VISIBLE
+                        binding.ivItemAlarmRemind1Icon.visibility = View.INVISIBLE
+                    } else {
+                        binding.ivItemAlarmRemind0Icon.visibility = View.INVISIBLE
+                        binding.ivItemAlarmRemind1Icon.visibility = View.VISIBLE
+                    }
+                    binding.ivItemAlarmOriginal0Icon.visibility = View.INVISIBLE
+                    binding.ivItemAlarmOriginal1Icon.visibility = View.INVISIBLE
+
+                    binding.tvItemAlarmTypeOriginal.visibility = View.INVISIBLE
+                    binding.tvItemAlarmTypeRemind.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
