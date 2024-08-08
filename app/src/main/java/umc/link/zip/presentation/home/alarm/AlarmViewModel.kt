6f97836cc6cc7960536a1ec20890fe67fac030f8
ekgml001 <dahea5069@gmail.com>
2024-08-09 02:22:22 +0900

    [FIX/#27] 최종의 최종

--- app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmViewModel.kt
+++ app/src/main/java/umc/link/zip/presentation/home/alarm/AlarmViewModel.kt
@@ -1,18 +1,33 @@
+package umc.link.zip.presentation.home.alarm
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import umc.link.zip.domain.model.alarm.Alarm
 import javax.inject.Inject
 
 @HiltViewModel
-class AlarmViewModel @Inject constructor(private val repository: AlarmRepository) : ViewModel() {
-    val alarms: StateFlow<List<Alarm>> = repository.alarms
+class AlarmViewModel @Inject constructor() : ViewModel() {
+    private val _alarms = MutableStateFlow<List<Alarm>>(emptyList())
+    val alarms: StateFlow<List<Alarm>> get() = _alarms
 
     fun updateAlarmStatus(alarmId: Int) {
         viewModelScope.launch {
-            repository.updateAlarmStatus(alarmId)
+            val updatedAlarms = _alarms.value.map {
+                if (it.id == alarmId) {
+                    it.copy(alarm_status = 1) // 상태를 1로 변경
+                } else {
+                    it
+                }
+            }
+            _alarms.value = updatedAlarms
         }
+    }
+
+    fun setAlarms(alarms: List<Alarm>) {
+        _alarms.value = alarms
     }
 }
