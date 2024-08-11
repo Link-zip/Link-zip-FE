6181c837636cf63746d91135dfa6e7c69c325026
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-08-11 17:45:54 +0900

    [feat/#26] Delete Zips Api 연결 완료

--- app/src/main/java/umc/link/zip/data/repositoryImpl/ZipRepositoryImpl.kt
+++ app/src/main/java/umc/link/zip/data/repositoryImpl/ZipRepositoryImpl.kt
@@ -3,13 +3,16 @@ package umc.link.zip.data.repositoryImpl
 import umc.link.zip.data.dto.BaseResponse
 import umc.link.zip.data.dto.zip.request.ZipCreateRequest
 import umc.link.zip.data.dto.zip.request.ZipEditRequest
+import umc.link.zip.data.dto.zip.request.ZipRmRequest
 import umc.link.zip.data.dto.zip.response.ZipCreateResponse
 import umc.link.zip.data.dto.zip.response.ZipEditResponse
 import umc.link.zip.data.dto.zip.response.ZipInquiryResponse
+import umc.link.zip.data.dto.zip.response.ZipRmResponse
 import umc.link.zip.data.service.ZipService
 import umc.link.zip.domain.model.zip.ZipCreateModel
 import umc.link.zip.domain.model.zip.ZipEditModel
 import umc.link.zip.domain.model.zip.ZipInquiryModel
+import umc.link.zip.domain.model.zip.ZipRmModel
 import umc.link.zip.domain.repository.ZipRepository
 import umc.link.zip.util.network.NetworkResult
 import umc.link.zip.util.network.handleApi
@@ -28,6 +31,10 @@ class ZipRepositoryImpl @Inject constructor(
 
     override suspend fun patchEditZip(editRequest: ZipEditRequest): NetworkResult<ZipEditModel> {
         return handleApi({ zipService.patchEditZip(editRequest) }) { response: BaseResponse<ZipEditResponse> -> response.result.toModel() }
+    }
+
+    override suspend fun deleteRmZip(rmRequest: ZipRmRequest): NetworkResult<ZipRmModel> {
+        return handleApi({ zipService.deleteRmZip(rmRequest) }) { response: BaseResponse<ZipRmResponse> -> response.result.toModel() }
     }
 }
 
--- app/src/main/java/umc/link/zip/data/service/ZipService.kt
+++ app/src/main/java/umc/link/zip/data/service/ZipService.kt
@@ -3,15 +3,18 @@ package umc.link.zip.data.service
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.Response
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.Query
 import umc.link.zip.data.dto.BaseResponse
 import umc.link.zip.data.dto.zip.request.ZipCreateRequest
 import umc.link.zip.data.dto.zip.request.ZipEditRequest
+import umc.link.zip.data.dto.zip.request.ZipRmRequest
 import umc.link.zip.data.dto.zip.response.ZipCreateResponse
 import umc.link.zip.data.dto.zip.response.ZipEditResponse
 import umc.link.zip.data.dto.zip.response.ZipInquiryResponse
+import umc.link.zip.data.dto.zip.response.ZipRmResponse
 
 interface ZipService {
     @POST("zips")
@@ -28,4 +31,9 @@ interface ZipService {
     suspend fun patchEditZip(
         @Query ("body") zipEditRequest: ZipEditRequest
     ) : Response<BaseResponse<ZipEditResponse>>
+
+    @DELETE("zips")
+    suspend fun deleteRmZip(
+        @Query ("body") zipRmRequest: ZipRmRequest
+    ) : Response<BaseResponse<ZipRmResponse>>
 }
