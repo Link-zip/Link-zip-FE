a463be540d516f717e904e98b2da37ce3273f128
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-08-11 17:21:42 +0900

    [feat/#26] 변수명 수정

--- app/src/main/java/umc/link/zip/data/service/ZipService.kt
+++ app/src/main/java/umc/link/zip/data/service/ZipService.kt
@@ -8,6 +8,7 @@ import retrofit2.http.Query
 import umc.link.zip.data.dto.BaseResponse
 import umc.link.zip.data.dto.zip.request.ZipCreateRequest
 import umc.link.zip.data.dto.zip.response.ZipCreateResponse
+import umc.link.zip.data.dto.zip.response.ZipInquiryResponse
 
 interface ZipService {
     @POST("zips")
