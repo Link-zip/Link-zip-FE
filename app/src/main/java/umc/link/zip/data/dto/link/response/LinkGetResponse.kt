64fbea41a38f74b19976f5ce7c68bb8053c05341
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-08-16 17:18:00 +0900

    [feat/#53] Link 데이터 가져오기 API 연동

--- app/src/main/java/umc/link/zip/data/dto/link/response/LinkGetResponse.kt
+++
@@ -1,8 +0,0 @@
-package umc.link.zip.data.dto.link.response
-
-import umc.link.zip.domain.model.link.LinkGetModel
-
-class LinkGetResponse : ArrayList<LinkGetItemResponse>(){
-    fun toModel() = LinkGetModel(
-    )
-}
--- app/src/main/java/umc/link/zip/domain/model/link/LinkGetModel.kt
+++
@@ -1,6 +0,0 @@
-package umc.link.zip.domain.model.link
-
-class LinkGetModel : ArrayList<LinkGetItemModel>(){
-    fun toModel() = LinkGetModel(
-    )
-}
