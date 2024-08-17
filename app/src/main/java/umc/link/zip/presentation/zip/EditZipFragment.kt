7e1c953182a7d8d5514ab1db351c9ff255d6dc16
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-08-18 02:37:13 +0900

    [feat/#53] Link/get_links API 연동중

---
+++ app/src/main/java/umc/link/zip/presentation/zip/EditZipFragment.kt
@@ -0,0 +1,17 @@
+package umc.link.zip.presentation.zip
+
+import dagger.hilt.android.AndroidEntryPoint
+import umc.link.zip.R
+import umc.link.zip.databinding.FragmentMakezipBinding
+import umc.link.zip.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class EditZipFragment : BaseFragment<FragmentMakezipBinding>(R.layout.fragment_editzip) {
+    override fun initObserver() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
