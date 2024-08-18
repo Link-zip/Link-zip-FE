0d589361cee9c5060b45bb8da7750d2a37b29f24
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2024-08-18 11:17:23 +0900

    [Feat/#55] Login API (#59)
    * [FEAT/#50] 닉네임 중복확인 API 연결
    
    * [ADD/#41] 회원가입 API 연결 중간저장
    
    * [ADD/#41] 회원가입 API 연결
    
    * [FEAT/#55] JWT 유효성 검증 구현
    
    * [ADD/#50] LoginActivity에서 ProfilesetFragment로 회원가입 키 전달
    
    * [MOD/#55] Login 테스트 -> 실제
    
    * [FIX/#50] ViewModel Instance 오류 수정
    
    * [MERGE/#55] 회원가입 API와 MERGE
    
    * [CHORE/#55] 필요없는 Log 삭제, JwtResponse kakaoId 변수 타입 변경
    
    ---------
    
    Co-authored-by: Timeisfast <seongyungim99@gamil.com>

--- app/src/main/java/umc/link/zip/presentation/login/LoginViewModel.kt
+++ app/src/main/java/umc/link/zip/presentation/login/LoginViewModel.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import umc.link.zip.data.dto.request.LoginRequest
+import umc.link.zip.domain.model.login.JwtModel
 import umc.link.zip.domain.model.login.LoginModel
 import umc.link.zip.domain.repository.LoginRepository
 import umc.link.zip.util.network.NetworkResult
@@ -20,10 +22,18 @@ class LoginViewModel @Inject constructor(
     private val _loginResult = MutableLiveData<NetworkResult<LoginModel>>()
     val loginResult : LiveData<NetworkResult<LoginModel>> = _loginResult
 
-    fun login() {
+    private val _checkJwtResult = MutableLiveData<NetworkResult<JwtModel>>()
+    val checkJwtResult : LiveData<NetworkResult<JwtModel>> = _checkJwtResult
+
+    fun login(request: LoginRequest) {
         viewModelScope.launch {
-            Log.d("login", "ViewModel 정상작동")
-            _loginResult.value = loginRepository.login()
+            _loginResult.value = loginRepository.login(request)
+        }
+    }
+
+    fun checkJwt() {
+        viewModelScope.launch {
+            _checkJwtResult.value = loginRepository.checkJwt()
         }
     }
 }
